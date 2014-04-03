273c721f1290769f20d410670a2a511d32572627
CannibalVox <sdbaynham@gmail.com>
2014-04-02 21:29:53 -0500

    Forgot to only get subblock part of metadata.

--- src/main/java/net/technic/technicblocks/blocks/DataDrivenBlock.java
+++ src/main/java/net/technic/technicblocks/blocks/DataDrivenBlock.java
@@ -252,7 +252,7 @@ public class DataDrivenBlock extends Block {
     @Override
     public ItemStack getPickBlock(MovingObjectPosition target, World world, int x, int y, int z)
     {
-        ItemStack result = new ItemStack(this, 1, world.getBlockMetadata(x,y,z));
+        ItemStack result = new ItemStack(this, 1, this.getSubBlock(world,x,y,z).getMetadata());
 
         for(ICreativePickerBehavior pickerBehavior : creativePickerBehaviors) {
             result = pickerBehavior.transformPickResult(target, world, x, y, z, result);
